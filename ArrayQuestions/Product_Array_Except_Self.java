0d59993810afa5c2530ed87393e10a6ec97896a2
Gaurav Kumar <95544839+NextGenGk@users.noreply.github.com>
2022-06-20 12:51:20 +0530

    Update all code and functions

--- ArrayQuestions/Product_Array_Except_Self.java
+++ ArrayQuestions/Product_Array_Except_Self.java
@@ -5,32 +5,33 @@ public class Product_Array_Except_Self {
 
     // Product Array Except Self Function
     public int[] productExceptSelf(int[] nums) {
-        // Create an empty array
-        int output[] = new int[ nums.length];
+        // Create an Array
+        int[] result = new int[nums.length];
 
-        // Initialize 0 (zero) index with the value of 1
-        output[0] = 1;
+        // Find right side product
+        result[nums.length - 1] = 1;
 
-        // left prefix product (Loop through nums.length)
-        for(int i=1;i<nums.length;i++){
-            output[i] = output[i-1] * nums[i-1];
+        // Iterate through right to left
+        for (int i = nums.length - 2; i >= 0; i--) {
+            // Find right side product each index
+            result[i] = result[i + 1] * nums[i + 1];
         }
 
-        // Initialize product to 1
-        int product = 1;
-
-        // Loop through right to left
-        for(int i=nums.length-1;i>=0;i--){
-            output[i] = output[i] * product;
-            product = product * nums[i];
+        // Declare left (temp) variable to hold left side product in each iteration
+        int left = 1;
+        for (int i = 0; i < nums.length; i++) {
+            // Find final result
+            result[i] = result[i] * left;
+            // Update left value at each iteration
+            left = left * nums[i];
         }
-        return output;
+        return result;
     }
 
-    // Main Function
+        // Main Function
     public static void main(String[] args){
         Product_Array_Except_Self pro = new Product_Array_Except_Self();
-        int[] arr = {4, 6, 1, 2};
+        int[] arr = {1,2,3,4};
         int[] result = pro.productExceptSelf(arr);
         for (int i=0;i< result.length; i++) {
             System.out.print(result[i] + " ");
@@ -38,38 +39,19 @@ public class Product_Array_Except_Self {
     }
 }
 // Output -
-// 12 8 48 24
+// 24 12 8 6
 
 // Algorithm -
 /*
 1. Create an empty array
-2. Initialize 0 (zero) index with the value of 1
-3. left prefix product
-4. Initialize product to 1
-5. Loop through right to left
+2. Find right side product
+3. Iterate through right to left & Find right side product each index
+4. Declare left (temp) variable to hold left side product in each iteration & Find final result
+5. Update left value at each iteration
  */
 
 // Time & Space Complexity -
 /*
 Time - O(n)
 Space - O(1)
- */
-
-// Another Solution -
-/*
-public int[] productExceptSelf(int[] nums) {
-    int[] result = new int[nums.length];
-
-    result[nums.length-1]=1;
-    for(int i=nums.length-2; i>=0; i--){
-        result[i]=result[i+1]*nums[i+1];
-    }
-
-    int left=1;
-    for(int i=0; i<nums.length; i++){
-        result[i]=result[i]*left;
-        left = left*nums[i];
-    }
-    return result;
-}
  */
